80ba7f4b8ce003c66eb924da4375c85bca60672a
baskooistra <bas.kooistra1986@gmail.com>
2019-07-16 16:29:10 +0200

    x

---
+++ src/Services/Love2u.IdentityProvider/Data/Love2uIdentityContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.Entities;
+using IdentityServer4.EntityFramework.Interfaces;
+using Love2u.IdentityProvider.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Love2u.IdentityProvider.Data
+{
+    public class Love2uIdentityContext : IdentityDbContext<User>
+    {
+        public Love2uIdentityContext(DbContextOptions<Love2uIdentityContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
